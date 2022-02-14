1c33aedde0d68d5425962a425ba1c17bfe1561f4
interpretapple-lab <ifsmetrics+interpretapple-lab@gmail.com>
2022-02-14 13:42:32 -0500

    Comments updated

--- examples/common.py
+++ examples/common.py
@@ -6,6 +6,11 @@ ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 
 ordinalltx = lambda n: r"%d$^{%s}$" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 
+
+# The following two methods, deskew and hog, are adapted versions of the methods given in the OpenCV tutorial entitled 'OCR of Hand-written Data using SVM' 
+# posted on 
+#       https://docs.opencv.org/4.5.3/dd/d3b/tutorial_py_svm_opencv.html
+
 def deskew(img, raster_sz = 20):
     affine_flags = cv.WARP_INVERSE_MAP|cv.INTER_LINEAR
     m = cv.moments(img)
@@ -27,103 +32,6 @@ def hog(img, bins_n = 16):
     hist = np.hstack(hists)     # hist is a 64 bit vector
     return hist
 
-# def img2VectorsWithInfluence(img, imgPro, imgCon, raster_sz = 20, ret_img_sz = 128):
-#     gx = cv.Sobel(img, cv.CV_32F, 1, 0)
-#     gy = cv.Sobel(img, cv.CV_32F, 0, 1)
-
-#     gxPro = cv.Sobel(imgPro, cv.CV_32F, 1, 0)
-#     gyPro = cv.Sobel(imgPro, cv.CV_32F, 0, 1)
-
-#     gxCon = cv.Sobel(imgCon, cv.CV_32F, 1, 0)
-#     gyCon = cv.Sobel(imgCon, cv.CV_32F, 0, 1)
-
-#     # Create a black image
-#     imgGradSize = ret_img_sz
-
-#     imgVectors = np.zeros((imgGradSize,imgGradSize,3), np.uint8)
-#     scaleFactor = imgGradSize/raster_sz
-    
-#     maxX = np.max(gx)
-#     maxY = np.max(gy)
-#     maxXY = max(maxX,maxY)
-
-#     maxXPro = np.max(gxPro)
-#     maxYPro = np.max(gyPro)
-#     maxXYPro = max(maxXPro,maxYPro)
-
-#     maxXCon = np.max(gxCon)
-#     maxYCon = np.max(gyCon)
-#     maxXYCon = max(maxXCon,maxYCon)
-#     i = 0
-    
-#     while i<raster_sz:
-#         j = 0
-#         while j<raster_sz:
-#             pt1 = (np.int32(i*scaleFactor),np.int32(j*scaleFactor))
-
-#             pt2 = (np.int32((i+gx[j][i]/maxXY)*scaleFactor), np.int32((j+gy[j][i]/maxXY)*scaleFactor))
-#             pt2Pro = (np.int32((i+gxPro[j][i]/maxXYPro)*scaleFactor), np.int32((j+gyPro[j][i]/maxXYPro)*scaleFactor))
-#             pt2Con = (np.int32((i+gxCon[j][i]/maxXYCon)*scaleFactor), np.int32((j+gyCon[j][i]/maxXYCon)*scaleFactor))
-
-#             r = 0
-#             g = 0
-#             b = 0
-#             if (gx[j][i] + gy[j][i] > 0):
-#                 if (gxPro[j][i] + gyPro[j][i] > 0):
-#                     g = 255
-#                     pt2Pro = (np.int32((i+(gx[j][i]/maxXY)*(gxPro[j][i]/maxXYPro))*scaleFactor), np.int32((j+(gy[j][i]/maxXY)*(gyPro[j][i]/maxXYPro))*scaleFactor))
-#                     imgVectors = cv.arrowedLine(imgVectors, pt1, pt2Pro, (r,g,b) ) 
-#                 elif (gxCon[j][i] + gyCon[j][i] > 0):
-#                     r = 255
-#                     pt2Con = (np.int32((i+(gx[j][i]/maxXY)*(gxCon[j][i]/maxXYCon))*scaleFactor), np.int32((j+(gy[j][i]/maxXY)*(gyCon[j][i]/maxXYCon))*scaleFactor))
-#                     imgVectors = cv.arrowedLine(imgVectors, pt1, pt2Con, (r,g,b) ) 
-#                 else:
-#                     r = 255
-#                     g = 255
-#                     b = 255
-#                     imgVectors = cv.arrowedLine(imgVectors, pt1, pt1, (r,g,b) ) 
-#             else:
-#                 if (gxPro[j][i] + gyPro[j][i] > 0):
-#                     r = 0
-#                     g = 255
-#                     b = 255
-#                     imgVectors = cv.arrowedLine(imgVectors, pt1, pt2Pro, (r,g,b) ) 
-#             j = j + 1
-#         i = i + 1
-
-
-#     return imgVectors
-
-# def img2Gradients(img, raster_sz = 20, ret_img_sz = 128):
-#     gx = cv.Sobel(img, cv.CV_32F, 1, 0)
-#     gy = cv.Sobel(img, cv.CV_32F, 0, 1)
-
-#     # Create a black image
-#     imgGradSize = ret_img_sz
-
-#     imgVectors = np.zeros((imgGradSize,imgGradSize,3), np.uint8)
-#     scaleFactor = imgGradSize/raster_sz
-    
-#     maxX = np.max(gx)
-#     maxY = np.max(gy)
-#     maxXY = max(maxX,maxY)
-
-#     i = 0
-    
-#     while i<raster_sz:
-#         j = 0
-#         while j<raster_sz:
-#             pt1 = (np.int32(i*scaleFactor),np.int32(j*scaleFactor))
-#             pt2 = (np.int32((i+gx[j][i]/maxXY)*scaleFactor), np.int32((j+gy[j][i]/maxXY)*scaleFactor))
-#             if (gx[j][i] + gy[j][i] > 0):
-#                 r = 255
-#                 g = 255
-#                 b = 255
-#                 imgVectors = cv.arrowedLine(imgVectors, pt1, pt2, (r,g,b) ) 
-#             j = j + 1
-#         i = i + 1
-
-#     return imgVectors
 
 
 def get_imgWithInfluence(img, raster_sz = 20, ret_img_sz = 128, fg_color=(0,0,0), bg_color=(255,255,255)):
